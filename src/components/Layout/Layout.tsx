20c0797a118fde68ff4abbe58013572050db9f07
Mauricio S Pereira <mauriciopspereira@MacBook-Pro-de-Mauricio.local>
2026-06-01 18:16:30 -0400

    Melhorias Cron v1.1.1

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -307,7 +307,7 @@ export const Layout = () => {
           
           {(isSidebarOpen || isMobileMenuOpen) ? (
             <div className="px-4 py-1 text-[10px] text-slate-400 font-mono flex items-center justify-between">
-              <span>v1.1.0c</span>
+              <span>v1.1.1</span>
               <span className="opacity-50">GM</span>
             </div>
           ) : (
